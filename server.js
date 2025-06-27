0dd8172e00dbf3a219928c1db3debbc950e85520
Musical Night <musicalnight25@gmail.com>
2025-06-27 16:02:31 +0530

    add swagger

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./shopping-api-swagger.json');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
@@ -7,11 +9,15 @@ const MONGO_URI = process.env.MONGO_URI;
 
 console.log('Connecting to MongoDB...');
 
+// Swagger Docs Route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB Connected');
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
+      console.log(`📚 Swagger docs: http://localhost:${PORT}/api-docs`);
     });
   })
   .catch((err) => {
